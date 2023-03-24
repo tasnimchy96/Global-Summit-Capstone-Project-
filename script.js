1dec5799bdcb02d2b8a878e3e7d46c6b1da945e9
tasnimchy96 <tasnim.achy96@gmail.com>
2023-03-25 03:20:37 +0600

    Add footer and menu toggle function in the about page

---
+++ script.js
@@ -0,0 +1,12 @@
+// Menu Toggle
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+document.querySelector(".open").addEventListener("click", openNav);
+document.querySelector(".close").addEventListener("click", closeNav);
